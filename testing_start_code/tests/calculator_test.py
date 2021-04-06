9a65f375d7fcb7022abe44f62cdcbfaae5e706f1
glilley54 <glilley54@gmail.com>
2021-04-06 17:13:58 +0100

    Homework Completed

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -3,4 +3,13 @@ from src.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-    self.assertEqual(5, add(2, 3))                                
+        self.assertEqual(5, add(2, 3))  
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10,7))
+
+    def test_divide(self):
+        self.assertEqual(5, divide(10,2))   
+
+    def test_multiply(self):
+        self.assertEqual(21, multiply(7,3))                              
